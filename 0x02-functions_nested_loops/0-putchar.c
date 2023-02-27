37debc06c2d065890679a526211a5f9777510ee3
Temitope Douglas <taymeedouglas07@gmail.com>
2023-02-27 08:21:25 -0800

    0x02-functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - print _putchar
+ * is written by Tope
+ * Return: 0
+ */
+
+int main(void)
+{
+_putchar('_');
+_puthcar('p');
+_puthcar('u');
+_puthcar('t');
+_puthcar('c');
+_puthcar('h');
+_puthcar('a');
+_puthcar('r');
+_puthcar('/n');
+return (0);
+}
