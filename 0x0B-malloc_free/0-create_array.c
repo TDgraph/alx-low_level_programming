1968b0295f1c04227dd2eda7b987a75e273086af
TDgraph <taymeedouglas07@gmail.com>
2023-06-13 13:13:29 -0700

    create array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - create array of size, assign char c
+ * @size: size of array
+ * @c: char to assign
+ * Description: creat array of size, assign char c
+ * Return: pointer to array, NULL if err
+ */
+
+char *create_array(unsigned int size, char c)
+
+{
+
+	char *str;
+
+	unsigned int a;
+
+
+
+	str = malloc(sizeof(char) * size);
+
+	if (size == 0 || str == NULL)
+
+		return (NULL);
+
+
+
+	for (a = 0; a < size; a++)
+		str[a] = c;
+
+	return (str);
+
+}
